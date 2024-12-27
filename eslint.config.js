a4fe915def2a2a6573d8e28a5b80ab485a57d40b
CreepyMemes <mjouins@gmail.com>
2024-12-27 11:40:23 +0100

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    ignores: ['dist/*'],
+  },
+
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-unused-vars': 'warn',
+    },
+  },
+
+  prettier,
+];
